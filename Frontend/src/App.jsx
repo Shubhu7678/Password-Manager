8048359a6978a193d6a64a112916ad25374c2cf1
Shubham <shubham.aswal@coderower.com>
2025-07-18 19:54:41 +0530

    first commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="h-screen flex items-center justify-center text-2xl font-bold text-blue-600">
+      Vite + React + Tailwind 🚀
+    </div>
+    </>
+  )
+}
+
+export default App
